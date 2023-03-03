d641295c4e0d710cae0a97ca092fbba14cf6718e
Welch Canavan <2304131+xiwcx@users.noreply.github.com>
2023-03-02 17:53:56 -0800

    add conditional prefix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,24 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { env } from "~/env.mjs";
+
+const productionCookiePrefix = "__Secure-";
+const sessionTokenCooke = "next-auth.session-token";
+
+/**
+ * next-auth prefaces cookie names differently in production.
+ *
+ * source: https://next-auth.js.org/configuration/options#cookies
+ */
+const getCookieName = () =>
+  env.NODE_ENV === "production"
+    ? productionCookiePrefix + sessionTokenCooke
+    : sessionTokenCooke;
 
 export function middleware(request: NextRequest) {
   const { cookies, nextUrl } = request;
 
-  if (!cookies.has("next-auth.session-token")) {
+  if (!cookies.has(getCookieName())) {
     const url = nextUrl.clone();
     url.pathname = "/api/auth/signin";
     url.searchParams.set("callbackUrl", nextUrl.href);
